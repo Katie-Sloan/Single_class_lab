95c67f39cd57d81d45938da64e503b7abbf5c196
Katie Sloan <katie.a.sloan@gmail.com>
2020-10-19 14:51:30 +0100

    Passed first test

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -1,1 +1,11 @@
+class Student:
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+
+    def talk(self):
+        return ("I can talk!")
+
+    def say_favourite_language(self, language):
+        return ("My favourite language is " + language)
 

136b8d5d053de6de142f22f55d37bb6c18e9d06f
Katie Sloan <katie.a.sloan@gmail.com>
2020-10-19 15:29:31 +0100

    Passed second test

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -1,7 +1,7 @@
 class Team:
     def __init__(self, name,  players, coach):
         self.name = name
-        self.players = []
+        self.players = players
         self.coach = coach
         self.points = 0
 

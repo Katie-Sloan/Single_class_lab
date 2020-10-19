d091f27b54ab14538dcbc2daba9e5a4f6f01da0f
Katie Sloan <katie.a.sloan@gmail.com>
2020-10-19 15:26:31 +0100

    Passed first test

---
+++ team_class/src/team.py
@@ -0,0 +1,20 @@
+class Team:
+    def __init__(self, name,  players, coach):
+        self.name = name
+        self.players = []
+        self.coach = coach
+        self.points = 0
+
+    def add_player(self, new_player):
+        self.players.append(new_player)
+
+    def has_player(self, name_of_player):
+        for player in players:
+            if player == name_of_player:
+                return True
+            else:
+                return False
+
+    def play_game(self, win, lose):
+        if win == True:
+            self.points += 3     
